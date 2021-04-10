d1b5809682c915560e037cf5a16e600702d595ca
vedaantAgarwal <vedaantagarwal267@gmail.com>
2021-04-10 21:07:59 +0530

    project1

---
+++ Slingshot.js
@@ -0,0 +1,23 @@
+class Slingshot {
+    constructor(object, point) {
+        var options = {
+            bodyA: object,
+            pointB: point,
+            length: 10,
+            stiffness: 0.5
+        }
+        this.sling = Matter.Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly() {
+        this.sling.bodyA = null;
+    }
+    display() {
+        if (this.sling.bodyA != null) {
+            var point1 = this.sling.bodyA.position;
+            var point2 = this.sling.pointB;
+            stroke("white");
+            line(point1.x, point1.y, point2.x, point2.y);
+        }
+    }
+}
